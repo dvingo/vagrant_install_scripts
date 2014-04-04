3f29662f216edaa0262b6e6d903a8124b6fed6e4
Dan Vingo <danvingo@gmail.com>
2014-04-04 18:00:31 -0400

    Update java.sh

--- java.sh
+++ java.sh
@@ -23,9 +23,10 @@
 
 tarball=/vagrant/jdk-7-linux-x64.tar.gz
 extract_name=jdk1.7.0_
-tar -xzf "$tarball" --directory /vagrant
+extract_dir='./'
+tar -xzf "$tarball" --directory $extract_dir
 mkdir -p /usr/lib/jvm
-mv /vagrant/$extract_name* /usr/lib/jvm/jdk1.7.0
+mv $extract_dir/$extract_name* /usr/lib/jvm/jdk1.7.0
 update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0/bin/java" 1
 update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0/bin/javac" 1
 update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0/bin/javaws" 1
